f890e7715181d7e0423dd9205bf03d123148c6b5
labprototypes <hello@prototypes.agency>
2025-08-14 09:32:13 +0200

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true
-};
-export default nextConfig;
+const nextConfig = { reactStrictMode: true };
+module.exports = nextConfig;
